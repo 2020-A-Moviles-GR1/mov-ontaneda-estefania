6d11518dcef08d24b088e9b4fbfbba0b20f8b012
Stefy Ontaneda <nicole.ontaneda@epn.edu.ec>
2020-06-25 00:54:54 -0500

    Clases Kotlin part1

--- 01-Kotlin/src/Main.kt
+++ 01-Kotlin/src/Main.kt
@@ -4,7 +4,8 @@ import java.util.function.Consumer
 import kotlin.collections.ArrayList
 
 fun main(args:Array<String>) {
-    //println("Hola")
+    //Varibles Mutables e inmutables
+    /*//println("Hola")
     //Int edadNico = 22;
 
     /*Mutable
@@ -42,17 +43,18 @@ fun main(args:Array<String>) {
     println(calcularSueldo(calculoEspecial = 2,sueldo=800.00,tasa=16.00))
     println(calcularSueldo(sueldo=500.00))
     imprimirMensaje()*/
+    */
 
-                                // ARREGLOS --- 15/06/2020
-    val arregloConstante: Array <Int> = arrayOf(1,2,3)
-    val arregloCumpleanos: ArrayList <Int> = arrayListOf(30,31,22,20)
-    println(arregloCumpleanos)
+    // ARREGLOS --- 15/06/2020
+    val arregloConstante: Array<Int> = arrayOf(1, 2, 3)
+    val arregloCumpleanos: ArrayList<Int> = arrayListOf(30, 31, 22, 20)
+    /*println(arregloCumpleanos)
     arregloCumpleanos.add(24)
     println(arregloCumpleanos)
     arregloCumpleanos.remove(30)
-    println(arregloCumpleanos)
+    println(arregloCumpleanos)*/
 
-                                //OPERADORES --- 15/06/2020
+    //OPERADORES --- 15/06/2020
     /*operadores de arreglos, se puede utilizar forEach se ejecuta dentro de las llaves la porción de código
     * Usada para iterar
     * Los operadores estan disponibles en todos los lenguajes
@@ -60,19 +62,18 @@ fun main(args:Array<String>) {
     * */
     arregloCumpleanos
             .forEach {
-            println("Valor de la iteración"+ it)
-        }
-
-    arregloCumpleanos
-            .forEach { valorIteracion: Int ->
-                println("Valor iteracion:" + valorIteracion)
+                //println("Valor de la iteración"+ it)
             }
 
     arregloCumpleanos
-            .forEach({
-                valorIteracion: Int ->
-                println("Valor iteracion:" + valorIteracion)
-                }
+            .forEach { valorIteracion: Int ->
+                //println("Valor iteracion:" + valorIteracion)
+            }
+
+    arregloCumpleanos
+            .forEach({ valorIteracion: Int ->
+                //println("Valor iteracion:" + valorIteracion)
+            }
             )
 
     //indice de la iteracion en ese momento //Sintaxis de más de una variable
@@ -80,14 +81,14 @@ fun main(args:Array<String>) {
     //Nos devuelve más de una variable se debe hacer
     arregloCumpleanos
             .forEachIndexed { index: Int, iteracion: Int ->
-                println("Valor de la iteracion" + iteracion)
+                // println("Valor de la iteracion" + iteracion)
             }
 
     val respuestaArregloForEach = arregloCumpleanos
-            .forEachIndexed { index:Int, iteracion:Int ->
-                println("Valor de la iteración" + iteracion)
+            .forEachIndexed { index: Int, iteracion: Int ->
+                //println("Valor de la iteración" + iteracion)
             }
-    println(respuestaArregloForEach)//no devuelve nada es -- > Unit
+    //println(respuestaArregloForEach)//no devuelve nada es -- > Unit
 
     //Quiero hacer negativo a un arreglo
     /*Map -> Muta el arreglo;
@@ -97,35 +98,97 @@ fun main(args:Array<String>) {
             .map { iterador: Int ->
                 iterador * -1
             }
-    println(respuestaMap)
-    println(arregloCumpleanos)
+    /*println(respuestaMap)
+    println(arregloCumpleanos)*/
 
-    val respuestaMap2:List<Int> = arregloCumpleanos
+    val respuestaMap2: List<Int> = arregloCumpleanos
             .map { iterador: Int ->
-                val nuevoValor:Int = iterador * -1
-                val otroValor:Int = nuevoValor * 2
+                val nuevoValor: Int = iterador * -1
+                val otroValor: Int = nuevoValor * 2
                 return@map otroValor
             }
-    println(respuestaMap2)
+    //println(respuestaMap2)
 
     /*--------- FILTER --------- Valores que me devuelvan los mayores a 23
         1)Devolver una expresion (TRUE OR FALSE)
         2)Nuevo arreglo que cumpla esa expresion
     */
-    val respuestaFilter: List <Int> = arregloCumpleanos
-            .filter {
-                iteracion: Int ->
+    val respuestaFilter: List<Int> = arregloCumpleanos
+            .filter { iteracion: Int ->
                 val mayor23: Boolean = iteracion > 23
                 return@filter mayor23
             }
 
     arregloCumpleanos
-            .filter{
-                iteracion: Int -> iteracion >23
-            }
+            .filter { it > 23 }
+    /* println("Arreglo con números mayores a 23" + respuestaFilter)
+    println("Arreglo original de cumpleaños"+ arregloCumpleanos)*/
 
-    println("Arreglo con números mayores a 23" + respuestaFilter)
-    println("Arreglo original de cumpleaños"+ arregloCumpleanos)
+    //******************    Clase 18/06/2020 ********************
+    /*ANY --> OR
+    * ALL --> AND
+    *  1) Devolver una expresión
+    *   2) Nos devuelve un booleano
+    *  */
+
+    val respuestaAny = arregloCumpleanos
+            .any { iterador: Int ->
+                return@any iterador < 25
+            }
+    /*Va a ver si hay al menos un elemento que sea menor que 25 y deberia devolvernos
+    Como si hay al menos 1 menor que 25 nos devuelve verdadero.*/
+    println("Operador ANY: " + respuestaAny)
+
+    val respuestaAll = arregloCumpleanos
+            .all {
+                iterador : Int ->
+                return@all iterador > 65
+            }
+    println("Operador all: " + respuestaAll)
+
+    /*Reduce
+    *  1) Devuelve el acumulado
+    *  2) En que valor empieza
+    * Los reduce se pueden ocupar con diferentes tipos de datos
+    * */
+    val respuestaReduce = arregloCumpleanos
+            //Acumulador 0
+            .reduce{acumulador, iteracion ->
+                return@reduce acumulador + iteracion
+            }
+    println("Operador reduce: "+respuestaReduce)
+
+    val respuestaReduceRight = arregloCumpleanos
+            //Acumulador 0
+            .reduceRight{acumulador, iteracion ->
+                return@reduceRight acumulador + iteracion
+            }
+    println("Operador reduceRight: "+respuestaReduceRight)
+    /*arregloCumpleanos.reduceRight
+    * arregloCumpleaños.foldRight
+    * */
+
+    //FOLD --> ACUMULADOR INICIADO EN UN VALOR DIFERENTE DE CERO
+    val respuestaFold = arregloCumpleanos
+            .fold(100,{acumulador,iteracion ->
+                return@fold acumulador - iteracion
+                    }
+                )
+    println("Operador fold: "+respuestaFold)
+
+    val respuestaFoldRight = arregloCumpleanos
+            .foldRight(100,{acumulador,iteracion ->
+                return@foldRight acumulador - iteracion
+            }
+            )
+    println("Operador foldRight: " + respuestaFoldRight)
+
+    /*Reducir el daño en 20% y menor a 18 no nos hace daño*/
+     val vidaActual = arregloCumpleanos
+             .map {it * 0.8}
+             .filter { it >18 }
+             .fold(100.00,{acumulador,iteracion->acumulador-iteracion})
+             .also { println("La vida actual que le queda es: " + it) }
 
 }//Cerrado del main
 
@@ -145,3 +208,30 @@ fun calcularSueldo(
 fun imprimirMensaje():Unit{ //Unit-->void
     println("Holisss.... :D")
 }
+
+ //             CLASES ABSTRACTAS --- 18/06/2020
+//Java implementation
+abstract class NumerosJava{
+     protected val numeroUno: Int
+     private val numeroDos: Int
+     constructor(uno:Int,dos:Int){
+         numeroUno=uno
+         numeroDos=dos
+     }
+ }
+
+//Kotlin implementation
+abstract class Numeros(
+        val numeroUno:Int,
+        val numeroDos: Int
+        )
+{}
+    //Para instanciar podemos usar val nuevosNumeros = Numeros(1,2)
+class Suma(
+            uno:Int,
+            dos: Int
+    ):Numeros(uno,dos){
+        fun sumar():Int{
+            return this.numeroUno + this.numeroDos
+        }
+    }
